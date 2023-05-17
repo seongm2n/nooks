bf6eca34874fd83f8ca163ec1b2683482fb69121
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-18 01:49:39 +0900

    feat : useNetwork 스타일 변경

--- src/useEffect/useNetwork.js
+++ src/useEffect/useNetwork.js
@@ -1,34 +1,42 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 //navigator가 online 또는 offline이 되는 것을 막아줌
 const useNetwork = (onChange) => {
-  const [status, setStatus] = useState(navigator.onLine);
-  const handleChange = () => {
-    if (typeof onChange === "function") {
-      onChange(navigator.onLine);
-    }
-    setStatus(navigator.onLine);
-  };
-  useEffect(() => {
-    window.addEventListener("online", handleChange);
-    window.addEventListener("offline", handleChange);
-    () => {
-      window.addEventListener("online", handleChange);
-      window.addEventListener("offline", handleChange);
-    };
-  }, []);
-  return status;
+	const [status, setStatus] = useState(navigator.onLine);
+	const handleChange = () => {
+		if (typeof onChange === 'function') {
+			onChange(navigator.onLine);
+		}
+		setStatus(navigator.onLine);
+	};
+	useEffect(() => {
+		window.addEventListener('online', handleChange);
+		window.addEventListener('offline', handleChange);
+		() => {
+			window.addEventListener('online', handleChange);
+			window.addEventListener('offline', handleChange);
+		};
+	}, []);
+	return status;
 };
 
 export const UseNetwork = () => {
-  const handleNetworkChange = (online) => {
-    console.log(online ? "We just went online" : "We are offline");
-  };
-  const onLine = useNetwork(handleNetworkChange);
-  return (
-    <div>
-      <h1>UseNetwork</h1>
-      <h2>{onLine ? "Online" : "Offline"}</h2>
-    </div>
-  );
+	const handleNetworkChange = (online) => {
+		console.log(online ? 'We just went online' : 'We are offline');
+	};
+	const onLine = useNetwork(handleNetworkChange);
+	return (
+		<div>
+			<h1>UseNetwork</h1>
+			<h2
+				style={{
+					border: '1.5px dotted green',
+					width: '80px',
+					borderRadius: '5px',
+				}}
+			>
+				{onLine ? 'Online' : 'Offline'}
+			</h2>
+		</div>
+	);
 };
